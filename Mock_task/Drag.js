e05c08fd62e613ca2f38b07327d91b01b33cecbe
Ambar <ambarnarwal@gmail.com>
2021-09-18 10:58:15 +0530

    Removing unnecessary image events

--- Mock_task/Drag.js
+++ Mock_task/Drag.js
@@ -70,51 +70,6 @@ d3.select("#img_1").call(d3.drag()
                 
     }))
 
-d3.select("#img_1F").call(d3.drag()
-    .on("start",function(event){
-                d3.select(this).style("border-width",0+"px")
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_s = d3.select(this).style("top")
-                left_img_s = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-start-order', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-                else{
-                    let temp_data_log = {'event_type':'drag-start-playground', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-       
-                })
-
-    .on("drag",function(event){
-                let i = event.x
-                let j = event.y
-                d3.select(this)
-                .style("top",j -75  + "px")
-                .style("left",i -180  +  "px")
-    })
-    .on("end",function(event){
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_e = d3.select(this).style("top")
-                left_img_e = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-end-order', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-                }
-
-                else{
-                    let temp_data_log = {'event_type':'drag-end-playground', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-            
-                }
-                d3.select(this).style("border-width",0+"px")
-                
-    }))
 
 d3.select("#img_2").call(d3.drag()
     .on("start",function(event){
@@ -346,51 +301,6 @@ d3.select("#img_4").call(d3.drag()
                 
     }))
 
-d3.select("#img_4F").call(d3.drag()
-    .on("start",function(event){
-                d3.select(this).style("border-width",0+"px")
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_s = d3.select(this).style("top")
-                left_img_s = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-start-order', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-                else{
-                    let temp_data_log = {'event_type':'drag-start-playground', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-       
-                })
-
-    .on("drag",function(event){
-                let i = event.x
-                let j = event.y
-                d3.select(this)
-                .style("top",j -75  + "px")
-                .style("left",i -180  +  "px")
-    })
-    .on("end",function(event){
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_e = d3.select(this).style("top")
-                left_img_e = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-end-order', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-                }
-
-                else{
-                    let temp_data_log = {'event_type':'drag-end-playground', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-            
-                }
-                d3.select(this).style("border-width",0+"px")
-                
-    }))
 
 d3.select("#img_5").call(d3.drag()
     .on("start",function(event){
@@ -668,51 +578,6 @@ d3.select("#img_B2").call(d3.drag()
                 
     }))
 
-d3.select("#img_B3").call(d3.drag()
-    .on("start",function(event){
-                d3.select(this).style("border-width",0+"px")
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_s = d3.select(this).style("top")
-                left_img_s = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-start-order', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-                else{
-                    let temp_data_log = {'event_type':'drag-start-playground', 'img_id':image_id, 'loc_top':top_img_s, 'loc_left':left_img_s, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });}
-       
-                })
-
-    .on("drag",function(event){
-                let i = event.x
-                let j = event.y
-                d3.select(this)
-                .style("top",j -75  + "px")
-                .style("left",i -180  +  "px")
-    })
-    .on("end",function(event){
-                let i = event.x
-                let j = event.y
-                image_id = d3.select(this).property("id")
-                top_img_e = d3.select(this).style("top")
-                left_img_e = d3.select(this).style("left")
-                time = Date.now()/1000;
-                if(i>1400){
-                    let temp_data_log = {'event_type':'drag-end-order', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-                }
-
-                else{
-                    let temp_data_log = {'event_type':'drag-end-playground', 'img_id':image_id, 'loc_top':top_img_e, 'loc_left':left_img_e, 'time':time};
-                    $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });
-            
-                }
-                d3.select(this).style("border-width",0+"px")
-                
-    }))
 
 /*************************************/
 //Images Mouseover and Mouseout
@@ -732,22 +597,6 @@ d3.select("#img_1").on("mouseout",function(){
     time = Date.now()/1000;
     let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
     $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-
-d3.select("#img_1F").on("mouseover",function(){
-        image_id = d3.select(this).property("id")
-        top_img_over = d3.select(this).style("top")
-        left_img_over = d3.select(this).style("left")
-        time = Date.now()/1000;
-        let temp_data_log = {'event_type':'mouseover', 'img_id':image_id, 'loc_top':top_img_over, 'loc_left':left_img_over, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-    
-d3.select("#img_1F").on("mouseout",function(){
-        image_id = d3.select(this).property("id")
-        top_img_out = d3.select(this).style("top")
-        left_img_out = d3.select(this).style("left")
-        time = Date.now()/1000;
-        let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
     
 d3.select("#img_2").on("mouseover",function(){
             image_id = d3.select(this).property("id")
@@ -829,22 +678,6 @@ d3.select("#img_4").on("mouseout",function(){
             let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
         $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
         
-d3.select("#img_4F").on("mouseover",function(){
-                image_id = d3.select(this).property("id")
-                top_img_over = d3.select(this).style("top")
-                left_img_over = d3.select(this).style("left")
-                time = Date.now()/1000;
-                let temp_data_log = {'event_type':'mouseover', 'img_id':image_id, 'loc_top':top_img_over, 'loc_left':left_img_over, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-            
-d3.select("#img_4F").on("mouseout",function(){
-                image_id = d3.select(this).property("id")
-                top_img_out = d3.select(this).style("top")
-                left_img_out = d3.select(this).style("left")
-                time = Date.now()/1000;
-                let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-
 d3.select("#img_5").on("mouseover",function(){
     image_id = d3.select(this).property("id")
     top_img_over = d3.select(this).style("top")
@@ -940,23 +773,7 @@ d3.select("#img_B2").on("mouseout",function(){
         time = Date.now()/1000;
         let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
         $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-    
-d3.select("#img_B3").on("mouseover",function(){
-            image_id = d3.select(this).property("id")
-            top_img_over = d3.select(this).style("top")
-            left_img_over = d3.select(this).style("left")
-            time = Date.now()/1000;
-            let temp_data_log = {'event_type':'mouseover', 'img_id':image_id, 'loc_top':top_img_over, 'loc_left':left_img_over, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-        
-d3.select("#img_B3").on("mouseout",function(){
-            image_id = d3.select(this).property("id")
-            top_img_out = d3.select(this).style("top")
-            left_img_out = d3.select(this).style("left")
-            time = Date.now()/1000;
-            let temp_data_log = {'event_type':'mouseout', 'img_id':image_id, 'loc_top':top_img_out, 'loc_left':left_img_out, 'time':time};
-        $('body').trigger({ 'type': 'data_log_event', 'event_data': temp_data_log });})
-        
+            
 /*************************************/
 
 /*************************************/
